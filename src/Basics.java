be2acf931ec2b91d7efc7c018bb0369e5cc6a0a0
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-05-31 23:18:37 +0545

    Initial Commit

--- src/Basics.java
+++ src/Basics.java
@@ -9,105 +9,105 @@ public class Basics {
 
     public static void main(String[] args) {
 
-//        System.out.println("2 + 2 = " + (2 + 2));
-//        System.out.println("2 + 2 = " + 2 + 2);
-//        System.out.println("2 + 2 = " + (2 + 2) + 2);
-//
-//        System.out.println("abc\ndef");
-//        System.out.println("abc\tdef");
-//        System.out.println("abc\rdef");
-//        System.out.println("abc\\ndef");
-//
-//        String name = "Sadikshya Acharya";
-//        System.out.println(name.substring(2));
-//        System.out.println(name.substring(0,9));
-//        System.out.println(name.length());
-//        System.out.println(name.indexOf("A"));
-//        System.out.println(name.charAt(0));
-//
-//
-//        String verbPhrase = "is money";
-//        System.out.println("Time " + verbPhrase);
+        System.out.println("2 + 2 = " + (2 + 2));
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2) + 2);
 
-//          int n = 3;
-//          n++;
-//          System.out.println("n == " + n);
-//          n--;
-//          System.out.println("n == " + n);
-//
-//          int num = (int) 3.9;
-//          System.out.println("num == " + num);
-//
-//          double celsius = 20;
-//          double fahrenheit;
-//          fahrenheit = ( 9 / 5) * celsius + 32.0;
-//          System.out.println(fahrenheit);
-//
-//          int result = 11;
-//          result /= 2;
-//          System.out.println("result is " + result);
-//
-//          int quotient, remainder;
-//          quotient = 7 / 3;
-//          remainder = 7 % 3;
-//          System.out.println(quotient);
-//          System.out.println(remainder);
-//
-//        double number = (1/3) * 3;
-//        System.out.println(number);
-//
-//        System.out.println("3x");
-//        System.out.println("3x + y");
-//        System.out.println("(x + y) / 7");
-//        System.out.println("(3x + y) / (z + 2)");
-//
-//        char a, b;
-//        a = 'b';
-//        System.out.println(a);
-//        b = 'c';
-//        System.out.println(b);
-//        a = b;
-//        System.out.println(a);
+        System.out.println("abc\ndef");
+        System.out.println("abc\tdef");
+        System.out.println("abc\rdef");
+        System.out.println("abc\\ndef");
+
+        String name = "Sadikshya Acharya";
+        System.out.println(name.substring(2));
+        System.out.println(name.substring(0,9));
+        System.out.println(name.length());
+        System.out.println(name.indexOf("A"));
+        System.out.println(name.charAt(0));
+
+
+        String verbPhrase = "is money";
+        System.out.println("Time " + verbPhrase);
+
+          int n = 3;
+          n++;
+          System.out.println("n == " + n);
+          n--;
+          System.out.println("n == " + n);
+
+          int num = (int) 3.9;
+          System.out.println("num == " + num);
+
+          double celsius = 20;
+          double fahrenheit;
+          fahrenheit = ( 9 / 5) * celsius + 32.0;
+          System.out.println(fahrenheit);
+
+          int result = 11;
+          result /= 2;
+          System.out.println("result is " + result);
+
+          int quotient, remainder;
+          quotient = 7 / 3;
+          remainder = 7 % 3;
+          System.out.println(quotient);
+          System.out.println(remainder);
+
+        double number = (1/3) * 3;
+        System.out.println(number);
+
+        System.out.println("3x");
+        System.out.println("3x + y");
+        System.out.println("(x + y) / 7");
+        System.out.println("(3x + y) / (z + 2)");
+
+        char a, b;
+        a = 'b';
+        System.out.println(a);
+        b = 'c';
+        System.out.println(b);
+        a = b;
+        System.out.println(a);
 
 //        double interest ;
 //        double balance;
 //        double rate;
 //        interest = balance * rate;
-
+//
 //        int distance;
 //        int time;
 //        distance = time * 80;
-
-
+//
+//
 //        double distance = 1.5;
 //        int count;
 //        count = 0;
-//
-//        int feet = 0;
-//        int inches = 0;
-//
-//        int numbers;
-//        int Number;
-//
-//    int sum;
-//    sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
-//    System.out.println("Sum of first ten positive numbers: " + sum);
-//
-//        int multiplication;
-//        multiplication = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
-//        System.out.println("Multiplication of first ten positive numbers: " + multiplication);
 
-//        double balance1 = 1000;
-//        double first = (5.0 / 100) * balance1;
-//        double second = first * ((5.0 /100) * balance1);
-//        double third = second * ((5.0 /100) * balance1);
-//        System.out.println(first);
-//        System.out.println(second);
-//        System.out.println(third);
+        int feet = 0;
+        int inches = 0;
 
-//        System.out.println("|---------|");
-//        System.out.println("|Sadikshya|");
-//        System.out.println("|---------|");
+        int numbers;
+        int Number;
+
+    int sum;
+    sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+    System.out.println("Sum of first ten positive numbers: " + sum);
+
+        int multiplication;
+        multiplication = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+        System.out.println("Multiplication of first ten positive numbers: " + multiplication);
+
+        double balance1 = 1000;
+        double first = (5.0 / 100) * balance1;
+        double second = first * ((5.0 /100) * balance1);
+        double third = second * ((5.0 /100) * balance1);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        System.out.println("|---------|");
+        System.out.println("|Sadikshya|");
+        System.out.println("|---------|");
 
         System.out.println("*     *        *          ******     *       *");
         System.out.println("*     *      *  *         *    *      *     *");
